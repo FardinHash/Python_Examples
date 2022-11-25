f9eae4606b735ae43e73b68649b322b9f0e8a525
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-11-25 20:16:24 +0600

    Create random_pass_gen.py

---
+++ Random password gen/random_pass_gen.py
@@ -0,0 +1,12 @@
+import random
+
+lowers = 'abcdefghijklmnopqrstuvwxyz'
+uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '012345678'
+symbols = '!@#$%^&*'
+
+combination = lowers + uppers + numbers + symbols
+length = 8
+password = ''.join(random.sample(combination, length))
+
+print(password)
