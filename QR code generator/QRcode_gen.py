9807a433708db7bc565db1f2259e5ca838794d3c
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-11-26 12:28:22 +0600

    Create QRcode_gen.py

---
+++ QR code generator/QRcode_gen.py
@@ -0,0 +1,13 @@
+import pyqrcode
+import png
+
+QRtext = input(print('Enter text to generate QR: '))
+
+QRimg = input(print('Enter image name to save: '))
+
+QRimg = QRimg + '.png'
+
+FinalQR = pyqrcode.create(QRtext)
+
+FinalQR.show()
+FinalQR.png(QRimg, scale=6)
